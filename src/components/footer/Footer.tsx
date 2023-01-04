25028a3b08f7e047d01a37a9f0e153be213a5e56
KaiserPhemi <kaiser.phemi@protonmail.ch>
2023-01-04 20:03:43 +0100

    feature()
    -- add links to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,36 +4,57 @@ import React from "react";
 // styles
 import styles from "./footer.module.css";
 
+// assets
+import logo from "../../assets/logo2.svg";
+
 // footer component
 const Footer = () => {
   return (
     <div className={styles.main_footer}>
-      <section>Shortly</section>
-      <section>Features Link Shortening Branded Links Analytics</section>
-      <section>Resources Blog Developers Support</section>
-      <section>Company About Our Team Careers Contact</section>
-      <section>Logos</section>
-      <section>
-        <div className={styles.attribution}>
-          Challenge by{" "}
-          <a
-            href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Frontend Mentor
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.frontendmentor.io/profile/KaiserPhemi"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Oluwafemi Akinwa
-          </a>
-          .
-        </div>
+      <section className={styles.logo}>
+        <img src={logo} alt="logo" />
       </section>
+      <section className={styles.link_wrapper}>
+        <section className={styles.link_section}>
+          <p>Features</p>
+          <p>Link Shortening</p>
+          <p>Branded Links</p>
+          <p>Analytics</p>
+        </section>
+        <section className={styles.link_section}>
+          <p>Resources</p>
+          <p>Blog</p>
+          <p>Developers</p>
+          <p>Support</p>
+        </section>
+        <section className={styles.link_section}>
+          <p>Company</p>
+          <p>About</p>
+          <p>Our Team</p>
+          <p>Careers</p>
+          <p>Contact</p>
+        </section>
+      </section>
+      <section>Logos</section>
+      {/* <div className={styles.attribution}>
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.frontendmentor.io/profile/KaiserPhemi"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Oluwafemi Akinwa
+        </a>
+        .
+      </div> */}
     </div>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,21 +4,28 @@ import React from "react";
 // styles
 import styles from "./navbar.module.css";
 
+// assets
+import logo from '../../assets/logo.svg';
+
 // nvaigation bar
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <section className={styles.navbar_links_wrapper}>
-        <section className={styles.logo}>Shortly</section>
+        <section className={styles.logo}>
+          <img src={logo} alt="logo" />
+        </section>
         <section className={styles.navbar_links}>
           <section className={styles.navbar_links_btn}>Features</section>
           <section className={styles.navbar_links_btn}>Pricing</section>
           <section className={styles.navbar_links_btn}>Resources</section>
         </section>
       </section>
-      <section>
-        <section>Login</section>
-        <section>Sign Up</section>
+      <section className={styles.btn_wrapper}>
+        <section className={styles.cta_btn}>Login</section>
+        <section className={`${styles.cta_btn} ${styles.cta_btn_signup}`}>
+          Sign Up
+        </section>
       </section>
     </div>
   );
